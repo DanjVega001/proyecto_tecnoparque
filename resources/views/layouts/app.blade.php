55e7d1e2c59e6b7dab7da3d4d0ef5f82caacad8c
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-19 20:30:40 -0500

    cracion de formularios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,15 +20,22 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+    integrity="sha384-GLhlTQ8i1IyZFf7R6Z75B6QQPGZ7I6tMOFYEMB+D5I4E2x4Ij2im/h3K2J5uWrM2"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- Scripts de Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <div id="app">
         <!-- Barra de navegación --> 
         @include('components.Header')
+
         <!-- Contenido principal -->
         <main class="py-4">
             @yield('content')

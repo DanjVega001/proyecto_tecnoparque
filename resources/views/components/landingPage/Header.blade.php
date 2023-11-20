f0d243748cd20ed6b70139315bc585693fc3fd07
DanielFonce1088 <jodani1570@gmail.com>
2023-11-20 16:22:38 -0500

    avance

--- resources/views/components/landingPage/Header.blade.php
+++ resources/views/components/landingPage/Header.blade.php
@@ -1,30 +1,39 @@
 <!-- resources/views/components/Header.blade.php -->
 
 <head>
-    
     <style>
-        /* Estilos para la línea verde */
+       <style>
+        
+        .btn-back span {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        .btn-back:hover {
+            transition: all .5s;
+            transform: rotate(-3deg) scale(1.1);
+            box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.6);
+        }
+
+        /* Estilos para la línea ROJA */
         .navbar-line {
-            background-color: #008000; /* Ajusté color verde  */
-            padding: 10px; /* Ajustar  relleno para abarcar más espacio */
-            margin-bottom: 20px; /* Ajustar el margen inferior según sea necesario */
+            background-color: #942339;
+            padding: 10px;
+            margin-bottom: 20px;
             display: flex;
             justify-content: space-between;
             align-items: center;
         }
 
-        /* Estilos para el logo */
-        .logo {
-            max-width: 1000%; /* Ajusta el ancho máximo del logo */
-        }
-
-        /* Estilo del nuevo botón */
-        .btn-custom {
-            background: #008000; /* Ajusté a un verde un poco más claro */
+        /* Estilo para los botones en la línea ROJA*/
+        .navbar-line button {
+            background: #942339;
             border: none;
             color: #fff;
-            font-size: 15px; /* Reduje el tamaño del texto */
-            letter-spacing: 2px; /* Ajusté el espaciado de letras */
+            font-size: 15px;
+            letter-spacing: 2px;
             font-family: 'Lato';
             font-weight: 600;
             outline: none;
@@ -33,20 +42,20 @@
             padding: 0px;
             overflow: hidden;
             transition: all .5s;
-            box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.4); /* Ajusté la sombra */
-            width: 150px; /* Reduje el ancho del botón */
-            height: 50px; /* Reduje la altura del botón */
+            box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.4);
+            width: 150px;
+            height: 50px;
         }
 
-        .btn-custom span {
+        .navbar-line button span {
             position: absolute;
             display: block;
         }
 
-        .btn-custom span:nth-child(1),
-        .btn-custom span:nth-child(2),
-        .btn-custom span:nth-child(3),
-        .btn-custom span:nth-child(4) {
+        .navbar-line button span:nth-child(1),
+        .navbar-line button span:nth-child(2),
+        .navbar-line button span:nth-child(3),
+        .navbar-line button span:nth-child(4) {
             height: 3px;
             width: 100%;
             background: linear-gradient(to right, rgba(0, 0, 0, 0), #f6e58d);
@@ -58,55 +67,50 @@
             0% {
                 transform: translateX(-100%);
             }
+
             100% {
                 transform: translateX(100%);
             }
         }
 
-        .btn-custom:hover {
+        .navbar-line button:hover {
             transition: all .5s;
             transform: rotate(-3deg) scale(1.1);
-            box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.6); /* Ajusté la sombra en hover */
+            box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.6);
         }
 
-        .btn-custom:hover span {
+        .navbar-line button:hover span {
             animation-play-state: paused;
         }
     </style>
- 
 </head>
 
-
-    <!-- Línea verde de navegación -->
-    <div class="navbar-line container-fluid">
-        <!-- Contenedor izquierdo con el logo -->
-        <div class="row">
-            <div class="col-sm-12 col-md-4">
-                <!-- Logo -->
-                <img src="{{ asset('img/logo.png') }}" alt="logo" class="logo">
-            </div>
-        </div>
-
-      <!-- Contenedor derecho con los botones -->
-<div class="col-sm-12 col-md-8 text-md-right">
-    <!-- Botón de Iniciar Sesión -->
-    <button type="button" class="btn-custom">Iniciar Sesión
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-    </button>
-    <!-- Botón de Registrarse -->
-    <button type="button" class="btn-custom">Registrarse
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-    </button>
-</div>
+<!-- Línea roja de navegación -->
+<div class="navbar-line container-fluid">
+    <!-- Contenedor izquierdo con el logo y el botón de regresar -->
+    <div class="row">
+        <div class="col-sm-12 col-md-4">
+          
         </div>
     </div>
 
+    <!-- Contenedor derecho con los botones -->
+    <div class="col-sm-12 col-md-8 text-md-right">
+        <!-- Botones de Iniciar Sesión y Registrarse -->
+        <button type="button" class="btn-custom">Registrarse
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <button type="button" class="btn-custom">Iniciar Sesión
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </div>
+</div>
     <!-- Scripts de Bootstrap y jQuery (asegúrate de incluir jQuery antes de Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>

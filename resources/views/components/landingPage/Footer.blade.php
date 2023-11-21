f74778b7428fb6a5e35220847756184cc1d77af5
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-21 12:31:16 -0500

    cambio de footer

--- resources/views/components/landingPage/Footer.blade.php
+++ resources/views/components/landingPage/Footer.blade.php
@@ -1,20 +1,11 @@
 <!-- resources/views/components/Footer.blade.php -->
 
 <footer>
-<img src="{{ asset('img/logo.png') }}" alt="logo" class="logo">
 
     <div class="social-icons-container">
         <a href="" class="social-icon bx bxl-facebook-circle"></a>
         <a href="" class="social-icon bx bxl-instagram"></a>
         <a href="" class="social-icon bx bxl-linkedin-square"></a>
-        <a href="" class="social-icon bx bxl-twitter"></a>
     </div>
-    <ul class="footer-menu-container">
-        <li class="menu-item">Legal</li>
-        <li class="menu-item">Cookies</li>
-        <li class="menu-item">Privacy</li>
-        <li class="menu-item">Shipping</li>
-        <li class="menu-item">Refounds</li>
-    </ul>
     <span class="copyright">&copy;2023, Tecno Parque. All rights reserved.</span>
 </footer>

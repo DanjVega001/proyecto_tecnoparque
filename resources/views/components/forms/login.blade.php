e395a267fecd96b10c95f7ac6f3d1007dc91fdbe
Zhawkys <cristhianzamor30@gmail.com>
2023-11-23 16:57:36 -0500

    registroForms_Login_css

--- resources/views/components/forms/login.blade.php
+++ resources/views/components/forms/login.blade.php
@@ -1,62 +1,58 @@
-@extends('layouts.app')
-
-@section('content')
-<section class="vh-100" style="background-color:  #3dac05;">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col col-xl-10">
-          <div class="card" style="border-radius: 1rem;">
-            <div class="row g-0">
-              <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-              </div>
-              <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                <div class="card-body p-4 p-lg-5 text-black">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   
-                  <form>
   
-                    <div class="d-flex align-items-center mb-3 pb-1">
-                      <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                      <span class="h1 fw-bold mb-0">Logo</span>
-                    </div>
-  
-                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-  
-                    <div class="form-outline mb-4">
-                      <input type="email" id="form2Example17" class="form-control form-control-lg" />
-                      <label class="form-label" for="form2Example17">Email address</label>
-                    </div>
-  
-                    <div class="form-outline mb-4">
-                      <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                      <label class="form-label" for="form2Example27">Password</label>
-                    </div>
-  
-                    <div class="pt-1 mb-4">
-                      <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
-                    </div>
-  
-                    <a class="small text-muted" href="#!">Forgot password?</a>
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
-                        style="color: #393f81;">Register here</a></p>
-                    <a href="#!" class="small text-muted">Terms of use.</a>
-                    <a href="#!" class="small text-muted">Privacy policy</a>
-
-                         <!-- Enlace para "Registrarse con Google" -->
-                         <div class="text-center">
-                            <a href="#" class="btn btn-info btn-lg mt-3">
-                                <i class="fab fa-google me-2"></i> Iniciar Seccion con Google
-                            </a>
-                        </div>
-                  </form>
-                
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <style>
+    body {
+      background-color: #942339;
+      color: white;
+    }
+    .login-box {
+      max-width: 400px;
+      margin: auto;
+      padding: 20px;
+      border-radius: 10px;
+      background-color: white;
+      margin-top: 50px;
+      color: #5e1325; /* Color gris para el texto */
+    }
+    .form-control, .btn {
+      border-radius: 20px;
+    }
+    .btn-primary {
+      background-color: #5e1325;
+      border-color: #5e1325;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="login-box">
+      <div class="text-center">
+        <img src="LOGO.png" alt="Logo" class="mb-3">
+        <h2>Inicio de Sesión</h2>
       </div>
+      <form>
+        <div class="form-group">
+          <label for="email" style="color: #5e1325;">Correo Electrónico</label>
+          <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo">
+        </div>
+        <div class="form-group">
+          <label for="password" style="color: #5e1325;">Contraseña</label>
+          <input type="password" class="form-control" id="password" placeholder="Contraseña">
+        </div>
+        <h6 style="color: #5e1325;">¿Haz olvidado la contraseña?</h6>
+        <button type="submit" class="btn btn-primary btn-block mt-3">Ingresar</button>
+      </form>
+      <button type="button" class="btn btn-primary btn-block mt-2">Registrar</button>
+      <hr>
+      <button type="button" class="btn btn-primary btn-block">Ingreso con Google</button>
     </div>
-  </section>
-  @endsection
+  </div>
+</body>
+</html>
--- resources/views/components/forms/registroUser.blade.php
+++ resources/views/components/forms/registroUser.blade.php
@@ -1,63 +1,70 @@
-
-@extends('layouts.app')
-@section('content')
-<section class="vh-100" style="background-color: #3dac05;">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col col-xl-10">
-                <div class="card" style="border-radius: 1rem;">
-                    <div class="row g-0">
-                        <div class="col-md-6 col-lg-5 d-none d-md-block">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                        </div>
-                        <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                            <div class="card-body p-4 p-lg-5 text-black">
-
-                                <form>
-
-                                    <div class="d-flex align-items-center mb-3 pb-1">
-                                        <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                                        <span class="h1 fw-bold mb-0">LLenar Formulario de Registro</span>
-                                    </div>
-
-                                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Crear una cuenta</h5>
-
-                                    <div class="form-outline mb-4">
-                                        <input type="email" id="form2Example17" class="form-control form-control-lg" />
-                                        <label class="form-label" for="form2Example17">Correo electrónico</label>
-                                    </div>
-
-                                    <div class="form-outline mb-4">
-                                        <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                                        <label class="form-label" for="form2Example27">Contraseña</label>
-                                    </div>
-
-                                    <div class="pt-1 mb-4">
-                                        <button class="btn btn-dark btn-lg btn-block" type="button">Registrarse</button>
-                                    </div>
-
-                                    <p class="mb-5 pb-lg-2" style="color: #393f81;">¿Ya tienes una cuenta? <a href="#!"
-                                            style="color: #393f81;">Inicia sesión</a></p>
-                                    <a href="#!" class="small text-muted">Términos de uso.</a>
-                                    <a href="#!" class="small text-muted">Política de privacidad</a>
-                                </form>
-
-                                <!-- Enlace para "Registrarse con Google" -->
-                                <div class="text-center">
-                                    <a href="#" class="btn btn-info btn-lg mt-3">
-                                        <i class="fab fa-google me-2"></i> Registrarse con Google
-                                    </a>
-                                </div>
-
-                            </div>
-                        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registro Usuarios</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- ICONOS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="{{ asset('css/forms/registroUser.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- HEADER -->
+            
+            <div class="container-fluid ">
+                <div class="row headerUp pt-2">
+                    <div class="col-4 text-center">
+                        <i class='bx bx-chevron-left'></i>
+                    </div>
+                    <div class="col-4 text-center">
+                        <p>REGISTRO USUARIOS</p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <i class='bx bx-dots-vertical-rounded'></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm lineaOsucra">
+                        .
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="container-fluid">
+                <div class="row contenidoForm">
+                <img src="{{ asset('img/astands/logoStand.png') }}" >
+                    <!-- Campos de Registro -->
+                    <form class="registration-form">
+                        <input type="text" placeholder="Nombre y Apellido" required>
+                        <input type="text" placeholder="Documento" required>
+                        <input type="email" placeholder="Email" required>
+                        <input type="tel" placeholder="Celular" required>
+                        <input type="text" placeholder="Dirección" required>
+                        <input type="date" placeholder="Fecha de Nacimiento" required>
+                        <select required>
+                            <option disabled selected>Género</option>
+                            <option value="male">Masculino</option>
+                            <option value="female">Femenino</option>
+                            <option value="other">Otro</option>
+                        </select>
+                        <input type="password" placeholder="Contraseña" required>
+                        <input type="password" placeholder="Confirmar Contraseña" required>
+                        <!-- Botones -->
+                        <button type="submit" class="register-button">Registrarse</button>
+                        <button type="button" class="login-button">Ingresar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</section>
-
-
-@endsection
+</body>
+</html>

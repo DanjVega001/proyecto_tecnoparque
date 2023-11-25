98c42d1c48ccb023dd65f1cad39bc06c5893c171
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-25 13:02:58 -0500

    /stand

--- resources/views/stands/index.blade.php
+++ resources/views/stands/index.blade.php
@@ -1,48 +1,100 @@
+<!-- resources/views/registro-stand.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-        <a href="{{route('stand.create')}}" class="btn btn-primary">Crear stand</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registro Stand</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- ICONOS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="user.css">
+    <link href="{{ asset('css/user/stand.css') }}" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body class="">
+    <div class="container-fluid">
+        
+        <div class="row">
+            <!-- HEADER -->
+            <div class="container-fluid  ">
+                <div class="row headerUp pt-2">
+                    <div class="col-4 text-center">
+                        <i class='bx bx-chevron-left'></i>
+                    </div>
+                    <div class="col-4 text-center d-flex justify-content-between align-items-center ">
+                        <button type="button" class="btn btn-link text-white" onclick="window.location.href='/user'">INICIO</button>
+                        <button type="button" class="btn btn-link text-white" onclick="window.location.href='/#'">VISITADOS</button>
+                    </div>
+                    
+                    <div class="col-4 text-center">
+                        <i class='bx bx-user'></i>
+                    </div>
+              
+                </div>
+                <div class="row">
+                    <div class="col-sm lineaOsucra">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Descripcion</th>
-                    <th>Facebook</th>
-                    <th>Instagram</th>
-                    <th>Tiktok</th>
-                    <th>Web</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-        @foreach ($stands as $stand)
-                <tr>
-                    <td>{{$stand->name}}</td>
-                    <td>{{$stand->description}}</td>
-                    <td>{{$stand->facebook}}</td>
-                    <td>{{$stand->instagram}}</td>
-                    <td>{{$stand->tiktok}}</td>
-                    <td>{{$stand->web}}</td>
-                    <td>
-                    <a href="{{ route('stand.edit', ['stand' => $stand->id ])  }}" class="btn btn-primary">Editar</a>
-                    <form action="{{ route('stand.destroy', ['stand' => $stand->id]) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que deseas eliminar esta empresa?');">
-                        @csrf
-                        @method('DELETE') 
-                        <button type="submit" class="btn btn-danger">Eliminar</button>
-                    </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        
+        <div class="">              
+            <div class="row ">
+                <div class="container-fluid contenidoCard">
+                    <div class="row">
+                        @foreach ($stands as $stand)
+                            <div class="col-md-4 mb-4">
+                                <!-- Mostrar calificación para cada stand -->
+                                <div class="calificacion">
+                                    <input type="radio" id="estrella{{ $stand->id }}5" name="calificacion{{ $stand->id }}" value="5">
+                                    <label for="estrella{{ $stand->id }}5">&#9733;</label>
+                                    <input type="radio" id="estrella{{ $stand->id }}4" name="calificacion{{ $stand->id }}" value="4">
+                                    <label for="estrella{{ $stand->id }}4">&#9733;</label>
+                                    <input type="radio" id="estrella{{ $stand->id }}3" name="calificacion{{ $stand->id }}" value="3">
+                                    <label for="estrella{{ $stand->id }}3">&#9733;</label>
+                                    <input type="radio" id="estrella{{ $stand->id }}2" name="calificacion{{ $stand->id }}" value="2">
+                                    <label for="estrella{{ $stand->id }}2">&#9733;</label>
+                                    <input type="radio" id="estrella{{ $stand->id }}1" name="calificacion{{ $stand->id }}" value="1">
+                                    <label for="estrella{{ $stand->id }}1">&#9733;</label>
+                                </div>
+                                <img class="card-img-top logoStand" src="{{ $stand->photo_url }}" alt="{{ $stand->company_name }}">
+                                <div class="card ">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $stand->company_name }}</h5>
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item"><i class="bx bx-map"></i> <strong>Dirección:</strong> {{ $stand->address }}</li>
+                                            <li class="list-group-item"><i class="bx bx-phone"></i> <strong>Teléfono:</strong> {{ $stand->phone_number }}</li>
+                                            <p class="card-text"><i class="bx bx-info-circle"></i> {{ $stand->description }}</p>
+                                            <li class="list-group-item"><i class="bx bx-link-external"></i> <strong>Sitio web:</strong> <a href="{{ $stand->website_url }}" target="_blank">{{ $stand->website_url }}</a></li>
+                                        </ul>
+
+                                        <div class="redes">          
+                                            <div class="social-icons mt-4  ">                                
+                                                <!-- Enlaces a redes sociales -->
+                                                <a href="{{ $stand->facebook }}" target="_blank" class="social-icon"><i class="bx bxl-facebook-circle bx-lg"></i></a>
+                                                <a href="{{ $stand->instagram }}" target="_blank" class="social-icon"><i class="bx bxl-instagram-alt bx-lg"></i></a>
+                                                <a href="{{ $stand->tiktok }}" target="_blank" class="social-icon"><i class="bx bxl-tiktok bx-lg"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-    
-       
+</body>
+</html>
+
 @endsection

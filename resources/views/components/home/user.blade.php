a36b7d2992fc7012890a76459a8966bf11938508
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-12-01 10:09:17 -0500

    12dqsdf

--- resources/views/components/home/user.blade.php
+++ resources/views/components/home/user.blade.php
@@ -1,4 +1,10 @@
-<div>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/quagga@0.12.1/dist/quagga.min.js"></script>
     <style>
         .scan-card {
             background: url('ruta/a/tu/imagen.gif') center center no-repeat;
@@ -16,7 +22,8 @@
             color: white; /* Ajusta el color del texto según sea necesario */
         }
     </style>
-
+</head>
+<body>
     <h4>Bienvenido, {{ Auth::user()->name }}!</h4>
     <p>Gracias por usar nuestra aplicación. Aquí encontrarás información sobre eventos y más.</p>
     <div class="card scan-card">
@@ -25,7 +32,6 @@
             Tu navegador no soporta la etiqueta de video.
         </video>
         <div class="card-body">
-
             <button class="btn btn-primary" id="scanQR" data-toggle="cameraButton">Abrir Cámara</button>
         </div>
     </div>
@@ -33,69 +39,77 @@
     <div class="card mt-3" id="video-container" style="display: none;">
         <!-- Agregado el elemento con id 'video-container' -->
     </div>
-</div>
 
-<script>
-    document.getElementById('scanQR').addEventListener('click', function () {
-        // Ocultar el botón al hacer clic
-        this.style.display = 'none';
+    <script>
+        document.getElementById('scanQR').addEventListener('click', function () {
+            // Ocultar el botón al hacer clic
+            this.style.display = 'none';
 
-        document.getElementById('video-container').style.display = 'block';
+            document.getElementById('video-container').style.display = 'block';
 
-        // Obtener el elemento de video
-        var video = document.getElementById('background-video');
+            // Obtener el elemento de video
+            var video = document.getElementById('background-video');
 
-        // Intentar acceder a la cámara
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                // Asignar el flujo de la cámara al elemento de video
-                video.srcObject = stream;
+            // Intentar acceder a la cámara
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    // Asignar el flujo de la cámara al elemento de video
+                    video.srcObject = stream;
 
-                // Inicializar QuaggaJS para la detección de códigos QR
-                Quagga.init({
-                    inputStream: {
-                        name: "Live",
-                        type: "LiveStream",
-                        target: video
-                    },
-                    decoder: {
-                        readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
-                    }
-                }, function (err) {
-                    if (err) {
-                        console.error('Error al inicializar QuaggaJS: ', err);
-                        return;
-                    }
+                    // Inicializar QuaggaJS para la detección de códigos QR
+                    Quagga.init({
+                        inputStream: {
+                            name: "Live",
+                            type: "LiveStream",
+                            target: video
+                        },
+                        decoder: {
+                            readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
+                        }
+                    }, function (err) {
+                        if (err) {
+                            console.error('Error al inicializar QuaggaJS: ', err);
+                            return;
+                        }
 
-                    // Iniciar la detección de códigos QR
-                    Quagga.start();
+                        // Iniciar la detección de códigos QR
+                        Quagga.start();
+                    });
+
+                    // Manejar el evento de detección de código QR
+                    Quagga.onDetected(function (result) {
+                        // Detener la detección para evitar múltiples redirecciones
+                        Quagga.stop();
+
+                        // Obtener el código QR reconocido
+                        var code = result.codeResult.code;
+
+                        // Verificar si la URL comienza con "http://" o "https://"
+                        if (code.startsWith("http://") || code.startsWith("https://")) {
+                            // Redirigir a la dirección del código QR
+                            window.location.href = code;
+                        } else {
+                            // Si no comienza con "http://" o "https://", puedes manejarlo según tus necesidades
+                            console.error('El código QR no es una URL válida:', code);
+                        }
+
+                        // Mostrar nuevamente el botón al detener la detección
+                        document.getElementById('scanQR').style.display = 'block';
+                    });
+                })
+                .catch(function (error) {
+                    console.error('Error al acceder a la cámara: ', error);
                 });
+        });
+    </script>
 
-                // Manejar el evento de detección de código QR
-                Quagga.onDetected(function (result) {
-                    // Detener la detección para evitar múltiples redirecciones
-                    Quagga.stop();
-
-                    // Obtener el código QR reconocido
-                    var code = result.codeResult.code;
-
-                    // Redirigir a la dirección del código QR
-                    window.location.href = code;
-
-                    // Mostrar nuevamente el botón al detener la detección
-                    document.getElementById('scanQR').style.display = 'block';
-                });
-            })
-            .catch(function (error) {
-                console.error('Error al acceder a la cámara: ', error);
-            });
-    });
-</script>
-
-<div class="card mt-3 list-card">
-    <div class="card-body">
-        <h5 class="card-title">Ver Listado de Stands</h5>
-        <p class="card-text">Explora el listado completo de stands disponibles.</p>
-        <a href="{{ route('stands.list') }}" class="btn btn-primary">Ir a Listado de Stands</a>
+    <div class="card mt-3 list-card">
+        <div class="card-body">
+            <h5 class="card-title">Ver Listado de Stands</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Explora nuestra selección</h6>
+            <p class="card-text">Explora el listado completo de stands disponibles. Encuentra la opción perfecta para tu evento.</p>
+            <a href="{{ route('stands.list') }}" class="btn btn-primary">Ir a Listado de Stands</a>
+        </div>
     </div>
-</div>
+</body>
+</html>

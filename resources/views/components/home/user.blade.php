69100f32d5a467cc27e171bee9cf9bec18f20b90
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-30 19:24:12 -0500

    camara_qr

--- resources/views/components/home/user.blade.php
+++ resources/views/components/home/user.blade.php
@@ -1,17 +1,8 @@
-<div>
+
     <style>
         .scan-card {
             position: relative;
             height: 200px;
-        }
-
-        .scan-card video {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
         }
 
         .scan-card .card-body {
@@ -22,12 +13,27 @@
             text-align: center;
             color: white;
         }
+
+        #video-container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: none;
+        }
+
+        #background-video {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
     </style>
 
     <h4>Bienvenido, {{ Auth::user()->name }}!</h4>
     <p>Gracias por usar nuestra aplicación. Aquí encontrarás información sobre eventos y más.</p>
+
     <div class="card scan-card">
-        <video autoplay muted playsinline id="scanner-video"></video>
         <div class="card-body">
             <h5 class="card-title">Ver Evaluación</h5>
             <p class="card-text">Accede para ver y completar la evaluación.</p>
@@ -35,47 +41,71 @@
         </div>
     </div>
 
-    <div class="card mt-3">
+    <div id="video-container" class="card scan-card">
+        <video autoplay muted id="background-video">
+            <source src="{{ asset('multimedia/videos/istockphoto-1314370116-640_adpp_is.mp4') }}" type="video/mp4">
+            Tu navegador no soporta la etiqueta de video.
+        </video>
         <div class="card-body">
-            <h5 class="card-title">Ver Listado de Stands</h5>
-            <p class="card-text">Explora el listado completo de stands disponibles.</p>
-            <a href="{{ route('stands.list') }}" class="btn btn-primary">Ir a Listado de Stands</a>
+            <!-- Contenido adicional para la cámara, si es necesario -->
         </div>
     </div>
-</div>
-
-@push('scripts')
-<script src="https://cdn.rawgit.com/serratus/quaggaJS/0.12.1/dist/quagga.min.js"></script>
-<script>
-    document.getElementById('scanQR').addEventListener('click', function () {
-        // Configuración básica para Quagga.js
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector('#scanner-video'),
-                constraints: {
-                    width: 480,
-                    height: 320,
-                    facingMode: "environment", // Utiliza la cámara trasera del dispositivo si está disponible
-                },
-            },
-            decoder: {
-                readers: ["code_128_reader"],
-            },
-        }, function (err) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            Quagga.start();
+    <script>
+        document.getElementById('scanQR').addEventListener('click', function () {
+            document.getElementById('video-container').style.display = 'block';
+    
+            // Obtener el elemento de video
+            var video = document.getElementById('background-video');
+    
+            // Intentar acceder a la cámara
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    // Asignar el flujo de la cámara al elemento de video
+                    video.srcObject = stream;
+    
+                    // Inicializar QuaggaJS para la detección de códigos QR
+                    Quagga.init({
+                        inputStream: {
+                            name: "Live",
+                            type: "LiveStream",
+                            target: video
+                        },
+                        decoder: {
+                            readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
+                        }
+                    }, function (err) {
+                        if (err) {
+                            console.error('Error al inicializar QuaggaJS: ', err);
+                            return;
+                        }
+    
+                        // Iniciar la detección de códigos QR
+                        Quagga.start();
+                    });
+    
+                    // Manejar el evento de detección de código QR
+                    Quagga.onDetected(function (result) {
+                        // Detener la detección para evitar múltiples redirecciones
+                        Quagga.stop();
+    
+                        // Obtener el código QR reconocido
+                        var code = result.codeResult.code;
+    
+                        // Redirigir a la dirección del código QR (ajusta según tus necesidades)
+                        window.location.href = code;
+                    });
+                })
+                .catch(function (error) {
+                    console.error('Error al acceder a la cámara: ', error);
+                });
         });
+    </script>
+    
+        <div class="card mt-3">
+            <div class="card-body">
+                <h5 class="card-title">Ver Listado de Stands</h5>
+                <p class="card-text">Explora el listado completo de stands disponibles.</p>
+                <a href="{{ route('stands.list') }}" class="btn btn-primary">Ir a Listado de Stands</a>
+            </div>
+        </div>
 
-        // Manejar el resultado del escaneo
-        Quagga.onDetected(function (result) {
-            alert('Código QR escaneado: ' + result.codeResult.code);
-            // Realiza acciones adicionales con el resultado del escaneo si es necesario
-        });
-    });
-</script>
-@endpush
